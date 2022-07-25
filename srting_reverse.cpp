15f8e846c27ec97716a405fe2f56edce44052e62
LOKESH PANDEY <73874665+lokeshs333@users.noreply.github.com>
2022-07-25 12:14:01 +0530

    Add files via upload

---
+++ srting_reverse.cpp
@@ -0,0 +1,12 @@
+
+string stringReverse(string S)
+{
+    // Write your code here.
+    int start = 0;
+    int end = S.size() - 1;
+    while (start < end)
+    {
+        swap(S[start++], S[end--]);
+    }
+    return S;
+}
---
+++ string_palindrome.cpp
@@ -0,0 +1,31 @@
+char tolowercase(char ch)
+{
+    if (ch >= 'a' && ch <= 'z' || (ch >= 0 && ch <= 9))
+        return ch;
+    else
+    {
+        ch = ch - 'A' + 'a';
+        return ch;
+    }
+}
+
+bool checkPalindrome(string s)
+{
+    // Write your code here.
+    int i = 0, j = s.length() - 1;
+    while (i <= j)
+    {
+        if (isalnum(s[i]) == 0)
+            i++;
+        else if (isalnum(s[j]) == 0)
+            j--;
+        else if (tolowercase(s[i]) != tolowercase(s[j]))
+            return false;
+        else
+        {
+            i++;
+            j--;
+        }
+    }
+    return true;
+}
